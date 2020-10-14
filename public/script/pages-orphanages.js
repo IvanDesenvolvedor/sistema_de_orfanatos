842aab8989a5aac00c98486506227df127118d4b
Ivan Teotonio <junioracioli@outlook.com>
2020-10-14 02:04:06 -0300

    Pagina do mapa criada

---
+++ public/script/pages-orphanages.js
@@ -0,0 +1,31 @@
+const map = L.map('mapid').setView([-27.222633,-49.6455874], 15)
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon =L.icon({
+    iconUrl:"./public/images/map-marker.svg",
+    iconSize:[58,68],
+    iconAnchor:[28,68],
+    popupAnchor:[170,2]
+})
+
+//create popoup overley
+const popup = L.popup({
+    closeButton:false,
+    className:"map-popup",
+    minWidth:240,
+    minHeight:240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>')
+
+L.marker([-27.222633,-49.6455874],{icon}).addTo(map)
+    .bindPopup(popup)
+    //.openPopup();
+
+
+
+
+
+/*L.marker([-27.222633,-49.6455874],{icon})
+.addTo(map)
+.bindPopup(popoup) */
